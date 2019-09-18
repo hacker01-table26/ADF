4305f5b916e992dd05e1399c148c151ef0d37f8d
hacker01-table26 <55465358+hacker01-table26@users.noreply.github.com>
2019-09-18 11:51:02 -0700

    Update run-pipeline-test.ps1

--- run-pipeline-test.ps1
+++ run-pipeline-test.ps1
@@ -1,3 +1,27 @@
+$resourceGroupName = "challenge2"
+$pipelineName = "CopySouthRidge"
+$dataFactoryName = "adfchallenge2"
+
 write-Output "Invoke Data Factory Pipeline Test for Southridge CosmosDb"
-$runId = Invoke-AzDataFactoryV2Pipeline -DataFactoryName "adfchallenge2" -ResourceGroupName "challenge2" -PipelineName "CopySouthRidge" -ParameterFile ".\southridge-pipeline-param.json"
+$runId = Invoke-AzDataFactoryV2Pipeline -DataFactoryName $dataFactoryName -ResourceGroupName $resourceGroupName -PipelineName $pipelineName -ParameterFile ".\southridge-pipeline-param.json"
 write-output "RunId: $runId"
+
+$exitCode = 1
+
+while ($True) {
+    $run = Get-AzDataFactoryV2PipelineRun -ResourceGroupName $resourceGroupName -DataFactoryName $DataFactoryName -PipelineRunId $runId
+
+    if ($run) {
+        if ($run.Status -ne 'InProgress') {
+            Write-Host "Pipeline run finished. The status is: " $run.Status -foregroundcolor "Yellow"
+            $run
+            
+            break
+        }
+        Write-Host  "Pipeline is running...status: InProgress" -foregroundcolor "Yellow"
+    }
+
+    Start-Sleep -Seconds 30
+}
+
+exit $exitCode
