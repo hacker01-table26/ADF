875a59601c96446be3bd46bf3d10b0879bd38b11
hacker01-table26 <55465358+hacker01-table26@users.noreply.github.com>
2019-09-18 11:41:21 -0700

    added unit test powershell file

---
+++ run-pipeline-test.ps1
@@ -0,0 +1,3 @@
+write-Output "Invoke Data Factory Pipeline Test for Southridge CosmosDb"
+$runId = Invoke-AzDataFactoryV2Pipeline -DataFactoryName "adfchallenge2" -ResourceGroupName "challenge2" -PipelineName "CopySouthRidge" -ParameterFile ".\southridge-pipeline-param.json"
+write-ouput "RunId: $runId"
