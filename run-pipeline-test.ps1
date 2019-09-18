5c6854e59c718f472849951f37d321b120879706
hacker01-table26 <55465358+hacker01-table26@users.noreply.github.com>
2019-09-18 11:58:11 -0700

    Update run-pipeline-test.ps1

--- run-pipeline-test.ps1
+++ run-pipeline-test.ps1
@@ -6,8 +6,6 @@ write-Output "Invoke Data Factory Pipeline Test for Southridge CosmosDb"
 $runId = Invoke-AzDataFactoryV2Pipeline -DataFactoryName $dataFactoryName -ResourceGroupName $resourceGroupName -PipelineName $pipelineName -ParameterFile ".\southridge-pipeline-param.json"
 write-output "RunId: $runId"
 
-$exitCode = 1
-
 while ($True) {
     $run = Get-AzDataFactoryV2PipelineRun -ResourceGroupName $resourceGroupName -DataFactoryName $DataFactoryName -PipelineRunId $runId
 
@@ -24,4 +22,9 @@ while ($True) {
     Start-Sleep -Seconds 30
 }
 
-Write-Error "Pipline failed with ExitCode $exitCode"
+if ($run.Status -ne "Succeeded")
+{
+    Write-Error "Pipline Unit Test Failed"    
+}
+
+
